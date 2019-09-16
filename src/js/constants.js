761690b8e8c579b3a5cbe9c50f1d2c6e339607c2
Gerasin Ivan <webartmotor@gmail.com>
2019-09-16 18:01:15 +0300

    feat: add constants

---
+++ src/js/constants.js
@@ -0,0 +1,5 @@
+export const GAME_SETTINGS_LOCAL_STORAGE = 'gameSettings';
+export const COMPLEXITY_DEFAULT = 'complexityDefault';
+export const COMPLEXITY_LOW = 'complexityLow';
+export const COMPLEXITY_MEDIUM = 'complexityMedium';
+export const COMPLEXITY_HARD = 'complexityHard';
